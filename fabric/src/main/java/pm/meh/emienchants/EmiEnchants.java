8ef46d308258e834d4aa2f46efcbdc02819b4bb6
Mephody <mephodio@gmail.com>
2024-06-09 18:58:24 +0300

    Initial commit

---
+++ fabric/src/main/java/pm/meh/emienchants/EmiEnchants.java
@@ -0,0 +1,18 @@
+package pm.meh.emienchants;
+
+import net.fabricmc.api.ModInitializer;
+
+public class EmiEnchants implements ModInitializer {
+    
+    @Override
+    public void onInitialize() {
+        
+        // This method is invoked by the Fabric mod loader when it is ready
+        // to load your mod. You can access Fabric and Common code in this
+        // project.
+
+        // Use Fabric to bootstrap the Common mod.
+        Common.LOG.info("Hello Fabric world!");
+        Common.init();
+    }
+}
